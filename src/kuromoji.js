f8d8738780498bcebd301413033d3bcd7fc744ae
Yannick Herrero <yannick.herrero@proton.me>
2026-04-23 09:07:13 +0000

    Remove DictionaryBuilder export
    DictionaryBuilder is a build-time tool for compiling IPAdic seed data
    into .dat.gz files. It is not needed at runtime in React Native apps
    and would add unnecessary code to the Metro bundle.

--- src/kuromoji.js
+++ src/kuromoji.js
@@ -18,15 +18,11 @@
 "use strict";
 
 var TokenizerBuilder = require("./TokenizerBuilder");
-var DictionaryBuilder = require("./dict/builder/DictionaryBuilder");
 
 // Public methods
 var kuromoji = {
     builder: function (option) {
         return new TokenizerBuilder(option);
-    },
-    dictionaryBuilder: function () {
-        return new DictionaryBuilder();
     }
 };
 
